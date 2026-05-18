8279b4f76ebe2631a01aed7ed227e3173a2f165f
Mykyta Shevchenko <sevcenkonik943@gmail.com>
2026-05-19 00:12:41 +0200

    Delete immigration-decision-support directory

--- immigration-decision-support/user-service/src/main/java/com/example/userservice/repository/FinancialLevelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.userservice.repository;
-
-import com.example.userservice.entity.FinancialLevel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FinancialLevelRepository extends JpaRepository<FinancialLevel, Integer> {
-}
